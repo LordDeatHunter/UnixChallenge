aafc6cc08f9bca8fd03ef112510f0d081e322a44
LordDeatHunter <desuhansa@gmail.com>
2026-03-28 17:59:59 +0100

    Search tag on click

--- web/src/components/TagsDisplay.tsx
+++ web/src/components/TagsDisplay.tsx
@@ -1,13 +1,38 @@
 import { Component, For } from "solid-js";
+import { useNavigate } from "@solidjs/router";
+import { setSearchQuery } from "@/store";
 
 interface TagsDisplayProps {
   tags: string[];
 }
 
-const TagsDisplay: Component<TagsDisplayProps> = (props) => (
-  <div class="tags-container">
-    <For each={props.tags}>{(tag) => <span class="tag">{tag}</span>}</For>
-  </div>
-);
+const TagsDisplay: Component<TagsDisplayProps> = (props) => {
+  const navigate = useNavigate();
+
+  const handleTagClick = (tag: string, e: MouseEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setSearchQuery(tag);
+    void navigate("/");
+  };
+
+  return (
+    <div class="tags-container">
+      <For each={props.tags}>
+        {(tag) => (
+          <span
+            class="tag"
+            classList={{
+              "tag-clickable": props.clickable !== false,
+            }}
+            onClick={(e) => handleTagClick(tag, e)}
+          >
+            {tag}
+          </span>
+        )}
+      </For>
+    </div>
+  );
+};
 
 export default TagsDisplay;
